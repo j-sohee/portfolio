66a0b9d6c6e925b410bf34b1de68dcb63271e1b4
j-sohee <sohee.jin@gmail.com>
2021-10-07 17:20:08 +0900

    gallery 호출

---
+++ js/myFlickr.js
@@ -0,0 +1,53 @@
+/*
+// key : c28561608d1c0e9f7db60ecfda79bf27
+//원하는 이미지 출력
+https://www.flickr.com/services/rest/?method=flickr.photos.search
+
+//사진경로
+https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg
+
+*/
+
+$.ajax({
+    url : "https://www.flickr.com/services/rest/?method=flickr.photos.search",
+    dataType : "json",
+    data : {
+        api_key : "c28561608d1c0e9f7db60ecfda79bf27",
+        per_page : 5,
+        format: "json",
+        nojsoncallback:1,
+        privacy_filter:5,
+        tags : "cloud"
+    }
+})
+.success(function(data){
+    console.log(data.photos.photo);
+    let items = data.photos.photo;
+
+    $(".gallery").append("<ul>");
+    $(items).each(function(index,data){
+
+        let text = data.title; 
+
+        $(".gallery ul")
+            .append(
+                $("<li>")
+                    .append(
+                        $("<a>").attr({
+                            href : "https://live.staticflickr.com/"+data.server+"/"+data.id+"_"+data.secret+"_b.jpg"
+                        })
+                            .append(
+                                $("<img>").attr({
+                                    src: "https://live.staticflickr.com/"+data.server+"/"+data.id+"_"+data.secret+"_m.jpg"
+                                })
+                            )
+                    )
+                    .append(
+                        $("<p>").text(text)
+                    )
+            )
+    })
+})
+.error(function(err){
+    console.err(err);
+})
