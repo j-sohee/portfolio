09b427849989ec4881a1f5272709bb1308f8851c
j-sohee <sohee.jin@gmail.com>
2021-10-20 22:00:19 +0900

    main scroll적용

--- js/main.js
+++ js/main.js
@@ -1,6 +1,39 @@
+//main scroll
+const $boxs = $(".myScroll");
+const $btns = $("#navi li");
+let posArr = [];
+let len = $btns.length;
+let baseLine = -300;
+
+for(let i=0; i<len; i++){
+    posArr.push($boxs.eq(i).offset().top);
+}
+
+$(window).on("scroll", function(){
+    let scroll = $(this).scrollTop();
+    for(let i=0; i<len; i++){
+        if(scroll >= posArr[i] +baseLine){
+            $btns.children("a").removeClass("on");
+            $btns.eq(i).children("a").addClass("on");
+
+            $boxs.removeClass("on");
+            $boxs.eq(i).addClass("on");
+        }
+    }
+});
+
+$("#navi li a").on("click", function(e){
+    e.preventDefault();
+    let target = $(this).attr("href");
+    let targetPos = $(target).offset().top;
+
+    $("html,body").animate({
+        scrollTop : targetPos
+    },1000);
+});
+
 //쿠키팝업----------------------------------------------
 let isCookie = document.cookie.indexOf("popup=done");
-console.log(isCookie);
 
 if(isCookie == -1){
     $("#popup").show();
