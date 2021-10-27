05bffde3ad9f4e8b54a82ff2799af0121d964deb
j-sohee <sohee.jin@gmail.com>
2021-10-27 23:20:28 +0900

    2depth 완성

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 //header 2depth menu
+
 $("#gnb>li").on("mouseenter", function(){
-    $(this).find(".sub").show();
+    $(this).find(".gnb_2depth").show();
 })
 $("#gnb>li").on("mouseleave", function(){
-    $(this).find(".sub").hide();
+    $(this).find(".gnb_2depth").hide();
 })
 
 $("#gnb>li").each(function(index){
     $("#gnb>li").eq(index).find("a").on("focusin", function(){
-        $("#gnb>li").eq(index).find(".sub").show();     
+        $("#gnb>li").eq(index).find(".gnb_2depth").show();     
     });
 
     $("#gnb>li").eq(index).find("a").last().on("focusout", function(){
-        $("#gnb>li").eq(index).find(".sub").hide();
+        $("#gnb>li").eq(index).find(".gnb_2depth").hide();
     });
 });
 
