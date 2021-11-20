8c0feb1a763db0f4825771d01885fce5e4f858c1
j-sohee <sohee.jin@gmail.com>
2021-11-20 13:45:13 +0900

    es6 적용

--- js/youtube.js
+++ js/youtube.js
@@ -1,106 +1,108 @@
 
-function Youtube(option){
-    this.init(option);
-    this.bindingEvent();
-}
-
-Youtube.prototype.init = function(option){
-
-    if(option.frame === undefined){
-        console.error("frame속성값은 필수 입력사항입니다.");
-        return;
+class Youtube{
+    constructor(option){
+        this.init(option);
+        this.bindingEvent();
     }
-    if(option.key == undefined){
-        console.error("key값은 필수 입력값입니다.");
-        return;
-    }
-    if(option.playlist === undefined){
-        option.playlist = "PL_R5V_bKthTtOskiIsA-w_VcyqRlaLdc0"
-    }
-    if(option.num === undefined){
-        opt.num = 6;
-    }
-
-
-    this.selector = option.frame;
-    this.playlist =  option.playList;
-    this.key = option.key;
-    this.num = option.num;
-}
-
-Youtube.prototype.bindingEvent = function(){
-    this.getYoutube();
-
-    //썸네일 클릭시 팝업호출
-    $("body").on("click", this.selector+" article a", function(e){
-        e.preventDefault();
-        this.cratePop(e.currentTarget);
-    }.bind(this));
-
-    $("body").on("click", ".vidpop span", function(){
-        $(".vidpop").remove();
-    })
-}
-
-Youtube.prototype.getYoutube = function(){
-    $.ajax({
-        url:"https://www.googleapis.com/youtube/v3/playlistItems",
-        dataType : "jsonp",
-        data : {
-            part : "snippet",
-            key : this.key,
-            maxResults : this.num,
-            playlistId : this.playlist
+    
+    init(option){
+    
+        if(option.frame === undefined){
+            console.error("frame속성값은 필수 입력사항입니다.");
+            return;
         }
-    })
-    .success(function(data){
+        if(option.key == undefined){
+            console.error("key값은 필수 입력값입니다.");
+            return;
+        }
+        if(option.playlist === undefined){
+            option.playlist = "PL_R5V_bKthTtOskiIsA-w_VcyqRlaLdc0"
+        }
+        if(option.num === undefined){
+            opt.num = 6;
+        }
     
-        let items = data.items;
+        this.selector = option.frame;
+        this.playlist =  option.playList;
+        this.key = option.key;
+        this.num = option.num;
+    }
     
-        $(items).each(function(_, data){
+    bindingEvent(){
+        this.getYoutube();
     
-            let txt = data.snippet.description; //본문
-            let tit = data.snippet.title; // 타이틀
-            let len = txt.length;
-            let titLen = tit.length;
-            let date = data.snippet.publishedAt;
-            date = date.split("T")[0];
+        //썸네일 클릭시 팝업호출
+        $("body").on("click", this.selector+" article a", e=>{
+            e.preventDefault();
+            this.cratePop(e.currentTarget);
+        });
     
-            if( len >  70){
-                txt = txt.substr(0, 80) + "...";
+        $("body").on("click", ".vidpop span", ()=>{
+            $(".vidpop").remove();
+        })
+    }
+    
+    getYoutube(){
+        $.ajax({
+            url:"https://www.googleapis.com/youtube/v3/playlistItems",
+            dataType : "jsonp",
+            data : {
+                part : "snippet",
+                key : this.key,
+                maxResults : this.num,
+                playlistId : this.playlist
             }
-            if( titLen > 50){
-                tit = tit.substr(0, 50) + "...";
-            }
-    
-            $(this.selector)
-                .append(
-                    $("<article>")
-                        .append(
-                            $("<a>").attr({ href : data.snippet.resourceId.videoId})
-                                    .append(
-                                        $("<img>").attr({ src : data.snippet.thumbnails.high.url })
-                                     ),
-                            $("<div class='btns'>")
+        })
+        .success(data=>{
+        
+            let items = data.items;
+        
+            $(items).each((_, data)=>{
+        
+                let txt = data.snippet.description; //본문
+                let tit = data.snippet.title; // 타이틀
+                let len = txt.length;
+                let titLen = tit.length;
+                let date = data.snippet.publishedAt;
+                date = date.split("T")[0];
+        
+                if( len >  70){
+                    txt = txt.substr(0, 80) + "...";
+                }
+                if( titLen > 50){
+                    tit = tit.substr(0, 50) + "...";
+                }
+        
+                $(this.selector)
+                    .append(
+                        $("<article>")
                             .append(
                                 $("<a>").attr({ href : data.snippet.resourceId.videoId})
-                                        .text("VIEW")
-                            ),
-                            $("<div class='con'>")
-                                    .append(
-                                        $("<h3>").text(tit),
-                                        $("<p>").text(txt)
-                                    )
-                        )
-                )
-        }.bind(this));
-    }.bind(this));
+                                        .append(
+                                            $("<img>").attr({ src : data.snippet.thumbnails.high.url })
+                                         ),
+                                $("<div class='btns'>")
+                                .append(
+                                    $("<a>").attr({ href : data.snippet.resourceId.videoId})
+                                            .text("VIEW")
+                                ),
+                                $("<div class='con'>")
+                                        .append(
+                                            $("<h3>").text(tit),
+                                            $("<p>").text(txt)
+                                        )
+                            )
+                    )
+            });
+        });
+        // .error(err=>{
+        //     console.error(err); 
+        // });
+    }
     
-}
-
-Youtube.prototype.cratePop = function(item){
-    let vidId = $(item).attr("href");
-    
+    cratePop(item){
+        let vidId = $(item).attr("href");
+        
         $("body")
             .append(
                 $("<div class='vidpop'>")
@@ -114,4 +116,6 @@ Youtube.prototype.cratePop = function(item){
                         $("<span>").text("close")
                     )
             )
+    }
 }
+
